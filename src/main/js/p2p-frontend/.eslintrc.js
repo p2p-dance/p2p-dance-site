432e3b0c81e55f7dc3237b8c0873464316e17695
Dalton Saffe <daltonsaffe@gmail.com>
2019-03-30 18:43:48 -0400

    add eslint config

---
+++ src/main/js/p2p-frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "extends": ["xo/esnext", "xo-nextjs/space"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
