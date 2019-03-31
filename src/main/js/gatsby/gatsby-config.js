b0965465745622cebab7ca962635476b6ed92009
Dalton Saffe <daltonsaffe@gmail.com>
2019-03-30 21:28:24 -0400

    dl gatsby readme

--- src/main/js/gatsby/gatsby-config.js
+++ src/main/js/gatsby/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `p2p.dance`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `dance music and open source software`,
     author: `@gatsbyjs`,
   },
   plugins: [
